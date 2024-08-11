109e9db14f0f2f2a6159e8827f72a6fb5a5b2ed4
Courville Software <software@courville.org>
2024-08-11 20:34:25 +0400

    Revert "Adapt codec PrivateData following libavformat behavior change for AV1"
    This reverts commit 6994e494f8a8bbd500d7fbcc5bd5f9fa79d90fa9.

--- Source/stream_parser_ffmpeg.c
+++ Source/stream_parser_ffmpeg.c
@@ -375,11 +375,8 @@ DBGP serprintf("\tPAR        %d/%d\r\n", codecpar->sample_aspect_ratio.num, code
 				
 				if( codecpar->extradata_size ) {
 					if( codecpar->extradata_size <= sizeof( video->extraData ) ) {
-						// libavformat used to skip the first 4 bytes in av1 private data
-						// hw codecs and our implementation using libavcodec still want this
-						int offset = ( video->format == VIDEO_FORMAT_AV1 ) ? 4 : 0 ;
-						video->extraDataSize = codecpar->extradata_size - offset ;
-						memcpy( video->extraData, codecpar->extradata + offset , video->extraDataSize );
+						video->extraDataSize = codecpar->extradata_size;
+						memcpy( video->extraData, codecpar->extradata, video->extraDataSize  );	
 						if( video->format == VIDEO_FORMAT_H264 && video->extraData[0] == 0x00 ) {
 serprintf("FF: parse H264 SPS\n");
 							// for non-AVCC H264, parse the SPS/PPS here

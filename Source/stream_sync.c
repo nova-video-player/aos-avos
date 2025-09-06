01f80fda45b7a1c8b24f5f82595be3044d8b1a1d
Courville Software <software@courville.org>
2025-09-06 15:18:54 +0200

    need DBG in stream_sync

--- Source/stream_sync.c
+++ Source/stream_sync.c
@@ -31,6 +31,8 @@
 #define DBGV2  	if(Debug[DBG_VID] > 1)
 #define DBGV3 	if(Debug[DBG_VID] > 2)
 
+#define DBG if( 0 )
+
 extern int stream_max_delay;
 extern int stream_no_sync;
 extern int stream_video_paused;

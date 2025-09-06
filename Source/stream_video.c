95c485c199f0925dcc9239da5a8d2e740824085d
Courville Software <software@courville.org>
2025-09-06 15:20:33 +0200

    return audio time if video is not valid (was there before)

--- Source/stream_video.c
+++ Source/stream_video.c
@@ -4549,7 +4549,9 @@ int stream_get_time_default( STREAM *s, int *total )
 	
 	if( total )
 		*total = s->duration;
-	return s->video_time;
+	int time = s->video->valid ? s->video_time : s->audio_time;
+DBGT serprintf( "sgct  pos: %8d  tot %d\r\n", time, total ? *total : -1 );
+	return time;
 }
 
 // *****************************************************************************

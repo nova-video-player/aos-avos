31d1141008e23e7e2cb4050b4d61bd9e97c0994a
Courville Software <software@courville.org>
2025-10-22 22:19:46 +0200

    Fix channel mask
    Solves the 6.1 audio not playing correctly (and probably 4.1 too).
    
    See https://github.com/nova-video-player/aos-AVP/issues/1318

--- Source/audio_interface_audiotrack_java.c
+++ Source/audio_interface_audiotrack_java.c
@@ -543,7 +543,7 @@ static int audiotrack_set_output_params(audio_ctx_t *at, int rate, int channels,
 
 	streamType = 3; /*STREAM_MUSIC*/
 	int sampleRateInHz = rate;
-	int channelConfig = track_chanmask << 2;
+	int channelConfig = track_chanmask;
 	int audioFormat = track_format;
 	mode = 1; /*MODE_STREAM*/
 
